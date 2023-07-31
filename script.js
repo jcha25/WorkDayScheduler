b8ad108b97bd01119a47f13cdcb2254901b49f0c
jcha25 <justicech25@gmail.com>
2023-07-31 13:31:47 -0500

    set up the rest of localStorage.getItem

--- script.js
+++ script.js
@@ -26,7 +26,15 @@ $(function () {
 
 
   $("#hour-9 textarea").val(localStorage.getItem("hour-9"))
-  
+  $("#hour-10 textarea").val(localStorage.getItem("hour-10"))
+  $("#hour-11 textarea").val(localStorage.getItem("hour-11"))
+  $("#hour-12 textarea").val(localStorage.getItem("hour-12"))
+  $("#hour-13 textarea").val(localStorage.getItem("hour-13"))
+  $("#hour-14 textarea").val(localStorage.getItem("hour-14"))
+  $("#hour-15 textarea").val(localStorage.getItem("hour-15"))
+  $("#hour-16 textarea").val(localStorage.getItem("hour-16"))
+  $("#hour-17 textarea").val(localStorage.getItem("hour-17"))
+
 
 
 });
