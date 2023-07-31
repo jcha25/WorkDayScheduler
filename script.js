31aba3baf8a76be0a299fa8320f1db74d132bee3
jcha25 <justicech25@gmail.com>
2023-07-31 13:26:11 -0500

    set up color functionality based on current time and id of each timeblock

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(function () {
+  var save = $(".save")
+  var timeBlock = $(".time-block")
+  var currentDay = $("#currentDay")
+  currentDay.text(dayjs().format("MMMM, DD, YYYY"))
+  
+  var hour = dayjs().hour()
+  timeBlock.each(function() {
+    var id = $(this).attr("id").slice(5)
+    if(hour < id) {
+      $(this).children(".description").attr("class", "col-8 col-md-10 description future")
+    } else if (hour > id) {
+      $(this).children(".description").attr("class", "col-8 col-md-10 description past")
+    } else {
+      $(this).children(".description").attr("class", "col-8 col-md-10 description present")
+    }
+  })
+
+
 
 });
